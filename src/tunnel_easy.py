455352f75a6c91a872f504b04f9d1e058829a6dd
Mike Hudak <mike@hudaknet.com>
2017-11-12 20:23:03 -0500

    Added a handler to simply echo packets back to sender

--- src/tunnel_easy.py
+++ src/tunnel_easy.py
@@ -1,5 +1,7 @@
 """Easy tunnel program"""
+import dpkt
 import logging
+import socket
 
 import tunnel
 
@@ -15,7 +17,17 @@ if __name__ == '__main__':
     t = tunnel.Tunnel(mode='tun')
 
     def echohandler(data):
-        """Echo the received data back to the sender"""
+        """Echo the received data back to the sender
+        but swap source and dest IPs"""
+        # Assume it's L3
+        leading_bytes = data[:4]
+        data = data[4:]  # Strip off the first 4 bytes
+        ip_pkt = dpkt.ip.IP(data)
+        src = ip_pkt.src
+        dst = ip_pkt.dst
+        ip_pkt.src = dst
+        ip_pkt.dst = src
+        data = leading_bytes + ip_pkt.__bytes__()
         t.send(data)
     t.set_rx_handler(echohandler)
     t.monitor()
